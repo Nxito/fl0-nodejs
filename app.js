aab8f6cb3acc3ba34371bcc604be6d44a997efdc
Nxito <55735074+Nxito@users.noreply.github.com>
2023-09-11 02:23:31 +0200

    Added Node red

--- app.js
+++ app.js
@@ -1,13 +1,68 @@
-const express = require('express');
-const app = express();
+var http = require("http");
+var express = require("express");
+var RED = require("node-red");
 const port = process.env.PORT ?? 3000;
+// Create an Express app
+var app = express();
 
-app.use(express.static('public'))
+// Add a simple route for static content served from 'public'
+app.use("/", express.static("public"));
 
-app.get('*', (req, res) => {
-    res.redirect('/');
-})
+// Create a server
+var server = http.createServer(app);
 
-app.listen(port, () => {
-    console.log(`App listening on port ${port}`);
-})
+// Create the settings object - see default settings.js file for other options
+var settings = {
+  httpAdminRoot: "/red/ad",
+  httpNodeRoot: "/api",
+  userDir: "./",
+  serialReconnectTime: 15000,
+  adminAuth: {
+    type: "credentials",
+    users: [
+      {
+        username: "admin",
+        password:
+          "$2b$10$M8SHpSeTUGQ/FhnfRKNKwuX9l.tyId1W69g5syK1ckkG.OcM1eSK6",
+        permissions: "*",
+      },
+    ],
+  },
+  editorTheme: {
+    page: {
+      title: "Nxito-Red",
+      css: __dirname + "/style.css",
+    },
+    header: {
+      title: "Nxito-Red",
+      image: __dirname + "/public/NXitoNEW RED.svg", // or null to remove image
+    },
+    login: {
+      // image: __dirname + "/static/logos/Nxito logo.png" // a 256x256 image
+    },
+    projects: {
+      // To enable the Projects feature, set this value to true
+      enabled: false,
+    },
+  },
+  functionGlobalContext: {
+    fs: require("fs"),
+    moment: require("moment"),
+    url: require("url"),
+    path: require("path"),
+  }, // enables global context
+};
+
+// Initialise the runtime with a server and settings
+RED.init(server, settings);
+
+// Serve the editor UI from /red
+app.use(settings.httpAdminRoot, RED.httpAdmin);
+
+// Serve the http nodes UI from /api
+app.use(settings.httpNodeRoot, RED.httpNode);
+console.log(`http://localhost:${port}/${RED.httpAdmin}`);
+
+server.listen(port);
+// Start the runtime
+RED.start();
